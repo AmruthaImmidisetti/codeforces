bbef75864224caddce1cc1266ae760004f56b35b
Amrutha Varshini Immidisetti <147792349+AmruthaImmidisetti@users.noreply.github.com>
2025-09-13 20:12:04 +0530

    Add Sublime Sequence [A] from Codeforces

---
+++ 2148/A - Sublime Sequence/solution.cpp
@@ -0,0 +1,1 @@
+#include <iostream>using namespace std; int main() {    int t;    cin >> t;    while (t--) {        int x, n;        cin >> x >> n;        if (n % 2 == 0)            cout << 0 << "\n";        else            cout << x << "\n";    }    return 0;}
