ea9ac2dba72786e42496281ef6f87ad5898bb430
Amrutha Varshini Immidisetti <147792349+AmruthaImmidisetti@users.noreply.github.com>
2026-02-13 20:28:46 +0530

    Update solution.cpp

--- 230/B - T-primes/solution.cpp
+++ 230/B - T-primes/solution.cpp
@@ -1,1 +1,34 @@
-#include <bits/stdc++.h>using namespace std; bool isPrime(long long x) {    if (x < 2) return false;    if (x % 2 == 0) return x == 2;    for (long long i = 3; i * i <= x; i += 2) {        if (x % i == 0) return false;    }    return true;} int main() {    int n;    cin >> n;    vector<long long> arr(n);    for (int i = 0; i < n; i++) cin >> arr[i];     for (int j = 0; j < n; j++) {        long double root = sqrtl(arr[j]);        long long sq = llround(root);         if (sq * sq == arr[j] && isPrime(sq)) {            cout << "YES" << endl;        } else {            cout << "NO" << endl;        }    }}
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPrime(long long x) {
+    if (x < 2) return false;
+    if (x % 2 == 0) return x == 2;
+
+    for (long long i = 3; i * i <= x; i += 2) {
+        if (x % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<long long> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    for (int j = 0; j < n; j++) {
+        long double root = sqrtl(arr[j]);
+        long long sq = llround(root);
+
+        if (sq * sq == arr[j] && isPrime(sq))
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
